6a34a82c3296ac837464830b0233944c70ad772d
Harshit <mishraharshit111@gmail.com>
2025-01-31 15:45:22 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextjs from "eslint-config-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const config = nextjs;
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default config;
